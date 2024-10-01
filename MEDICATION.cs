239d6aae2a1d67c4d607d9decb74d023068038b0
Molina2409 <105228031+Molina2409@users.noreply.github.com>
2024-09-30 22:46:02 -0500

    Add files via upload

---
+++ MEDICATION.cs
@@ -0,0 +1,55 @@
+﻿
+
+using NurseNotes.Context;
+using NurseNotes.Model;
+
+namespace Medication.repositorio
+{
+    public interface IMedicationRepository
+    {
+        Task<List<Medication>> GetAll();
+        Task<Medication> GetMedication(int MED_ID);
+        Task<Medication> CreateMedication(int MED_ID, string MEDDSC, int STOCK);
+        Task<Medication> UpdateMedication(Medication medication);
+        Task<Medication> DeleteMedication(int MED_ID);
+    }
+}
+
+
+
+    public class MedicationRepository : IMedicationRepository
+    {
+        private readonly TestDbNurseNotes _db;
+
+        public MedicationRepository(TestDbNurseNotes db)
+        {
+            _db = db;
+        }
+
+        public  Task<Medication> CreateMedication(int MED_ID, string MEDDSC, int STOCK)
+        {
+        throw new NotImplementedException();
+    }
+
+        public  Task<Medication> UpdateMedication(Medication medication)
+        {
+        _      throw new NotImplementedException();
+    }
+
+        public  Task<Medication> DeleteMedication(int MED_ID)
+        {
+        throw new NotImplementedException();
+    }
+
+        public  Task<Medication> GetMedication(int MED_ID)
+        {
+        throw new NotImplementedException();
+    }
+
+        public  Task<List<Medication>> GetAll()
+        {
+        throw new NotImplementedException();
+    }
+     
+}
+
---
+++ PERXGROUP.cs
@@ -0,0 +1,53 @@
+﻿
+using NurseNotes.Context;
+using NurseNotes.Model;
+
+
+namespace PerxGroup.repositorio
+{
+    public interface IPerXGroupRepository
+    {
+        Task<List<PerXGroup>> GetAll();
+        Task<PerXGroup> GetPerXGroup(int PG_ID);
+        Task<PerXGroup> CreatePerXGroup(int PG_ID, int GRP_ID, int PER_ID);
+        Task<PerXGroup> UpdatePerXGroup(PerXGroup perXGroup);
+        Task<PerXGroup> DeletePerXGroup(int PG_ID);
+    }
+}
+
+
+    public class PerXGroupRepository : IPerXGroupRepository
+    {
+        private readonly TestDbNurseNotes _db;
+
+        public PerXGroupRepository(TestDbNurseNotes db)
+        {
+            _db = db;
+        }
+
+        public  Task<PerXGroup> CreatePerXGroup(int PG_ID, int GRP_ID, int PER_ID)
+        {
+        throw new NotImplementedException();
+    }
+
+        public  Task<PerXGroup> UpdatePerXGroup(PerXGroup perXGroup)
+        {
+        throw new NotImplementedException();
+    }
+
+        public  Task<PerXGroup> DeletePerXGroup(int PG_ID)
+        {
+        throw new NotImplementedException();
+    }
+
+        public  Task<PerXGroup> GetPerXGroup(int PG_ID)
+        {
+        throw new NotImplementedException();
+    }
+
+        public  Task<List<PerXGroup>> GetAll()
+        {
+        throw new NotImplementedException();
+    }
+    }
+}
